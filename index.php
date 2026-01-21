dd158d13e4aa4fe199676f20705f19ae036c49d8
support-helpline <sammuelwilling@gmail.com>
2026-01-21 06:26:00 -0600

    Update index.php

--- index.php
+++ index.php
@@ -93,18 +93,7 @@ $keywords = "urban gardening, micro garden, balcony garden, rooftop garden, city
 <!doctype html>
 <html lang="en">
 <head>
-   <!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,5004001,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5004001&101" alt="statistics" border="0"></a></noscript>
-<!-- Histats.com  END  -->
+  
   <meta charset="utf-8">
   <title><?php echo h($pageTitle); ?></title>
 
@@ -353,6 +342,18 @@ hs.src = ('//s10.histats.com/js15_as.js');
     <a href="tel:<?php echo h($phone); ?>"><?php echo h($phone); ?></a>
   </div>
 </footer>
-
+ <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5004001,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5004001&101" alt="statistics" border="0"></a></noscript>
+<!-- Histats.com  END  -->
 </body>
 </html>
+
